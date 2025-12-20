9d2a35009a727c5c0f8dd85aaa831e541bd6dc3f
Krruddy <rkinsiclounon@gmail.com>
2025-12-20 22:06:52 +0000

    Corrected the names of the Terraform variables.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -35,7 +35,7 @@ variable "ssh_user" {
   default     = "user"
 }
 
-variable "ssh_public_key_ruddy_dektop" {
+variable "ssh_public_key_ruddy_desktop" {
   description = "Ruddy's desktop Public SSH key to be added to the VMs."
   type        = string
   sensitive   = true # Prevents Terraform from showing the key in plan outputs
