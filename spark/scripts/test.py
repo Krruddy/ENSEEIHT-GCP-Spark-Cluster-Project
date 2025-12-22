b73478073fa5ec2e91232e8078be2d6af8f59e6c
Krruddy <rkinsiclounon@gmail.com>
2025-12-22 21:37:37 +0000

    Add Spark job execution playbook and update test script for dynamic inputs
    - Create `run_spark_job.yml` playbook to automate Spark job execution, including input data processing, HDFS output management, and result archiving.
    - Update `test.py` script to accept dynamic input and output file paths via command line arguments.

--- spark/scripts/test.py
+++ spark/scripts/test.py
@@ -1,3 +1,4 @@
+import sys
 from pyspark.sql import SparkSession
 import socket
 
@@ -5,10 +6,20 @@ spark = SparkSession.builder.appName("SanityCheck").getOrCreate()
 
 print(f"I am running on node: {socket.gethostname()}")
 
+# Get output file name from command line argument
+if len(sys.argv) < 3:
+    print("Usage: spark-submit test.py <output_file_name> <data_file_name>")
+    sys.exit(1)
+
+output_name = sys.argv[1][1:]
+data_file = sys.argv[2][1:]
+
 try:
-    df = spark.read.text("hdfs://spk-mst-01:9000/input/lorem_ipsum.txt")
+    df = spark.read.text(f"hdfs://spk-mst-01:9000/{data_file}")
     print(f"Read {df.count()} lines from HDFS.")
+    df.write.mode("overwrite").text(f"hdfs://spk-mst-01:9000/{output_name}")
+
 except Exception as e:
-    print(f"HDFS Read Failed: {e}")
+    print(f"HDFS Operation Failed: {e}")
 
 spark.stop()
